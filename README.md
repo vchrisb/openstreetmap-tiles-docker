67311fb0d0c4755f752f1172a8f1112b522eac65
vchrisb <vchrisb@users.noreply.github.com>
2018-02-01 17:44:37 +0100

    initial

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# openstreetmap-tiles-docker
+# OpenStreetMap Tile Server Container
+
+This repository contains instructions for building a
+[Docker](https://www.docker.io/) image containing the OpenStreetMap tile
+serving software stack.  It is based on the
+[Switch2OSM instructions](https://switch2osm.org/manually-building-a-tile-server-16-04-2-lts/).
+
+As well as providing an easy way to set up and run the tile serving software it
+also provides instructions for managing the back end database, allowing you to:
+
+* Import OSM data into the database
+
+
+Run `docker run -e GIS_HOST=x.x.x.x -e GIS_USER=postgres -e GIS_DATABASE=gis -e GIS_PASSWORD=password -p 80:80 -d --name osm vchrisb/openstreetmap-tiles`.
+
+## About
+
+The container runs Ubuntu 14.04 (Trusty) and is based on the
+[phusion/baseimage-docker](https://github.com/phusion/baseimage-docker).  It
+includes:
+
+* Apache 2.2
+* The latest [Osm2pgsql](http://wiki.openstreetmap.org/wiki/Osm2pgsql) code (at
+  the time of image creation)
+* The latest [Mapnik](http://mapnik.org/) code (at the time of image creation)
+* The latest [Mod_Tile](http://wiki.openstreetmap.org/wiki/Mod_tile) code (at
+  the time of image creation)
---
+++ run.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+##
+# Run OpenStreetMap tile server operations
+#
+
+# Command prefix that runs the command as the web user
+asweb="setuser www-data"
+
+die () {
+    msg=$1
+    echo "FATAL ERROR: " msg > 2
+    exit
+}
+
+_startservice () {
+    sv start $1 || die "Could not start $1"
+}
+
+import () {
+    # Find the most recent import.pbf or import.osm
+    import=$( ls -1t /data/import.pbf /data/import.osm 2>/dev/null | head -1 )
+    test -n "${import}" || \
+        die "No import file present: expected /data/import.osm or /data/import.pbf"
+
+    echo "Importing ${import} into gis"
+    echo "$OSM_IMPORT_CACHE" | grep -P '^[0-9]+$' || \
+        die "Unexpected cache type: expected an integer but found: ${OSM_IMPORT_CACHE}"
+
+    number_processes=`nproc`
+
+    # Limit to 8 to prevent overwhelming pg with connections
+    if test $number_processes -ge 8
+    then
+        number_processes=8
+    fi
+
+    $asweb osm2pgsql --slim --hstore --cache $OSM_IMPORT_CACHE --database gis --number-processes $number_processes $import
+}
+
+cli () {
+    echo "Running bash"
+    cd /var/www
+    exec bash
+}
+
+startservices () {
+    _startservice renderd
+    _startservice apache2
+}
+
+_wait () {
+    WAIT=$1
+    NOW=`date +%s`
+    BOOT_TIME=`stat -c %X /etc/container_environment.sh`
+    UPTIME=`expr $NOW - $BOOT_TIME`
+    DELTA=`expr 5 - $UPTIME`
+    if [ $DELTA -gt 0 ]
+    then
+	sleep $DELTA
+    fi
+}
+
+# Unless there is a terminal attached wait until 5 seconds after boot
+# when runit will have started supervising the services.
+if ! tty --silent
+then
+    _wait 5
+fi
+
+# Execute the specified command sequence
+for arg
+do
+    $arg;
+done
+
+# Unless there is a terminal attached don't exit, otherwise docker
+# will also exit
+if ! tty --silent
+then
+    # Wait forever (see
+    # http://unix.stackexchange.com/questions/42901/how-to-do-nothing-forever-in-an-elegant-way).
+    tail -f /dev/null
+fi
